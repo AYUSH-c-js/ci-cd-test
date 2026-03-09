62c9022cbfa13e2cd363717ad898f96cc03f6f28
Ayush-c-js <parmarayuah@gmail.com>
2026-03-09 20:11:22 +0530

    initial backend api

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+// First API
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello from EC2 API" })
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
